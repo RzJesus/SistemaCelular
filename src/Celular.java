b0a5e932d67e9a365b3598170232a8dcedd7e03b
Jesus Ruiz <jesusgabrielzr03@gmail.com>
2026-08-30 14:01:01 -0500

    "Agregue constructor con parametros"

--- src/Celular.java
+++ src/Celular.java
@@ -8,6 +8,11 @@ public class Celular {
         marca = "sin marca";
         modelo = "Sin modelo";
         precio = 0.0;
+    }
+    public Celular(String marca, String modelo, double precio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = marca;
 
     }
 
