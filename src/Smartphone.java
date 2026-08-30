ebd4716b4233383bba12a6fd26a0cdc90e1b5b32
Jesus Ruiz <jesusgabrielzr03@gmail.com>
2026-08-30 14:13:06 -0500

    "Cree el constructor de Smartphone.java"

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,5 +1,12 @@
 public class Smartphone extends Celular {
     String sistemaOperativo;
     int memoriaRam;
+    public Smartphone(String marca, String  modelo, double precio, String sistemaOperativo, int memoriaRam){
+
+        super(marca, modelo, precio);
+
+        this.sistemaOperativo = sistemaOperativo;
+        this.memoriaRam = memoriaRam;
+    }
 
 }
